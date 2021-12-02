31548f511237505d91fb7524c382485a89f59d8b
Eduardo Poiate <eduardopoiate@gmail.com>
2021-12-02 16:06:52 -0300

    Creating CustomUser

--- DTT/users/admin.py
+++ DTT/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 # Register your models here.
+
+
+@admin.register(get_user_model())
+class Users(admin.ModelAdmin):
+    """Admin for CustomUser"""
--- DTT/users/views.py
+++ DTT/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView as DefaultLoginView, LogoutView as DefaultLogoutView
 
 # Create your views here.
+
+
+class LoginView(DefaultLoginView):
+    pass
+
+
+class LogoutView(DefaultLogoutView):
+    pass
