9ec1aba0ff04904f440a44f677f38f320246a64b
Eduardo Poiate <eduardopoiate@gmail.com>
2021-12-07 12:38:54 -0300

    Creating WorkDay model

--- DTT/work/models.py
+++ DTT/work/models.py
@@ -7,6 +7,12 @@ class WorkUnit(models.Model):
 
     activity = models.ForeignKey(to='activities.Activity', on_delete=models.CASCADE)
     worker = models.ForeignKey(to='users.CustomUser', on_delete=models.CASCADE)
-    date = models.DateTimeField()
+    date = models.ForeignKey(to='work.WorkDay', on_delete=models.CASCADE)
     duration = models.DurationField()
+
+
+class WorkDay(models.Model):
+
+    date = models.DateTimeField()
+
 
