ec52477c87906f896619ab62ecae5c5175817dbc
Eduardo Poiate <eduardopoiate@gmail.com>
2021-12-07 12:29:19 -0300

    Updating Activity model

--- DTT/activities/models.py
+++ DTT/activities/models.py
@@ -4,5 +4,9 @@ from django.db import models
 
 
 class Activity(models.Model):
-    pass
+
+    project = models.ForeignKey(to='projects.Project', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
+
 
